6deebdac39147c55b8094d2aa1deeccde3edb88d
Jane Choi <55301593+jaeyoung-jane-choi@users.noreply.github.com>
2021-03-21 20:59:50 +0900

    Update [20210321]Webcrawling_with_Beautifulsoup.py

--- [20210321]Webcrawling_with_Beautifulsoup.py
+++ [20210321]Webcrawling_with_Beautifulsoup.py
@@ -45,35 +45,35 @@ import matplotlib.pyplot as plt
 # print(df.shape)
 
 
-# df = pd.DataFrame(columns= ['comment','star'])
-#
-# for k in range(1,100):
-#     try:
-#         print('This is the page we are looking at')
-#         print(k)
-#         url = "https://movie.naver.com/movie/point/af/list.nhn?st=mcode&sword=187310&target=after&page={}".format(k)
-#         html = requests.get(url).text
-#         soup = BeautifulSoup(html, 'html.parser')
-#         elements = soup.find_all('td', class_ = 'title')
-#         print('element count')
-#         print(len(elements))
-#         for i in range(len(elements)):
-#             stars = elements[i].select_one('em').text
-#             comment = elements[i].select_one('br').next_sibling.strip()
-#             df = df.append({'comment': comment, 'star': stars}, ignore_index=True)
-#         interval = round(random.uniform(0, 2), 2)
-#         print('This is the interval')
-#         print(interval)
-#         time.sleep(interval)
-#     except:
-#         print('This will not happen')
+df = pd.DataFrame(columns= ['comment','star'])
 
-# print(df.head())
-# print(df.shape)
-# print(df.tail())
+for k in range(1,100):
+    try:
+        print('This is the page we are looking at')
+        print(k)
+        url = "https://movie.naver.com/movie/point/af/list.nhn?st=mcode&sword=187310&target=after&page={}".format(k)
+        html = requests.get(url).text
+        soup = BeautifulSoup(html, 'html.parser')
+        elements = soup.find_all('td', class_ = 'title')
+        print('element count')
+        print(len(elements))
+        for i in range(len(elements)):
+            stars = elements[i].select_one('em').text
+            comment = elements[i].select_one('br').next_sibling.strip()
+            df = df.append({'comment': comment, 'star': stars}, ignore_index=True)
+        interval = round(random.uniform(0, 2), 2)
+        print('This is the interval')
+        print(interval)
+        time.sleep(interval)
+    except:
+        print('This will not happen')
 
-# df.to_csv('./naver_movie.csv', index=False)
-# df.to_excel('./naver_movie.xlsx')
+print(df.head())
+print(df.shape)
+print(df.tail())
+
+df.to_csv('./naver_movie.csv', index=False)
+df.to_excel('./naver_movie.xlsx')
 
 df = pd.read_csv('./naver_movie.csv')
 print(df.head())
